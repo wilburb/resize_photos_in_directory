1f79914967a2bcb0e38597e49293c32dfcd88e8e
Benjamin Wilbur <wilburb@wilburweb.com>
2012-08-20 16:53:40 -0500

    Added new photos path as the new photos needed to be in a different directory.

--- resize_photos.py
+++ resize_photos.py
@@ -3,16 +3,16 @@ import Image
 
 sizes = [100, 200, 300]
 
-def read_directory(path):
+def read_directory(path, new_path):
     # create new directories to store resized photos
     for size in sizes:
-        size_dir = path+'_'+str(size)
+        size_dir = new_path+'_'+str(size)
         if not os.path.exists(size_dir):
             os.makedirs(size_dir)
     for dirname, dirs, files in os.walk(path):
         for infile in files:
             for size in sizes:
-                outfile = os.path.join(path+'_'+str(size), infile)
+                outfile = os.path.join(new_path+'_'+str(size), infile)
                 infile_full = os.path.join(path, infile)
                 resize_photo(infile_full, outfile, size)
 
@@ -24,4 +24,5 @@ def resize_photo(original_photo, new_photo, size):
     except IOError:
         print "cannot create thumbnail for '%s'" % original_photo
 
-read_directory('')
+#final 'photos' will be appended with _size for each size needed
+read_directory('/path/to/photos/dir', '/path/to/resized_photos/dir/photos') 
