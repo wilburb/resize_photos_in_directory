e7fd60ac1e27c063f7f0ad7824d6cd82b30525d8
Benjamin Wilbur <wilburb@wilburweb.com>
2012-08-20 16:01:42 -0500

    removed readme from top of script file

--- resize_photos.py
+++ resize_photos.py
@@ -1,21 +1,3 @@
-'''
-Photo resizing script
-@author wilburb@wilburweb.com
-
-Purpose: Create multiple image sizes for all images in a directory.
-
-Description: read through all photos in a directory (not recursively)
-			 resizing each to the specified widths and storing in a
-			 new directory with name corresponding to new widths.
-			 (ie, photos run with sizes = [100, 200] becomes photos, 
-			 	  photos_100, and photos_200)
-			 Aspect ratio is maintained, and the dimensions given in 
-			 the sizes array/list are for width only.
-Usage: place the full path to your photo directory in 
-       read_directory('') and put all desired sizes
-       in the sizes array/list
-'''
-
 import os, sys
 import Image
 
